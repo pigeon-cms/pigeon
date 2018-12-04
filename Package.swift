2b534bbeea9b10319ca6ec6d16ddd9a4c8c8608f
Hal Lee <hal@lee.me>
2018-12-04 08:35:24 -0500

    Empty project init

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Pigeon",
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"])
+    ]
+)
