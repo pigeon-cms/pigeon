e9886102b5c4e2363f2aa7d374111f78bbe64f01
Hal Lee <hal@hal-lee.com>
2018-12-05 22:06:02 -0500

    Basic auth implemented

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -1,8 +1,14 @@
 import Vapor
+import Authentication
 import FluentPostgreSQL
 
 struct User: Content, PostgreSQLUUIDModel, Migration {
     var id: UUID?
     private(set) var email: String
     private(set) var password: String
+}
+
+extension User: BasicAuthenticatable {
+    static let usernameKey: WritableKeyPath<User, String> = \.email
+    static let passwordKey: WritableKeyPath<User, String> = \.password
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,13 +1,19 @@
 import Vapor
+import Authentication
 
 public func routes(_ router: Router) throws {
     let userRouteController = UserController()
     try userRouteController.boot(router: router)
 
-    router.get { req -> EventLoopFuture<View> in
+    let basicAuthMiddleware = User.basicAuthMiddleware(using: BCrypt)
+    let guardAuthMiddleware = User.guardAuthMiddleware()
+    let basicAuthGroup = router.grouped([basicAuthMiddleware, guardAuthMiddleware])
+
+    basicAuthGroup.get { req -> EventLoopFuture<View> in
+//        let user = try req.requireAuthenticated(User.self)
         return try generateVueRoot(for: req)
     }
-
+    
     router.get("/json") { _ in
         // TODO
         return "json"
