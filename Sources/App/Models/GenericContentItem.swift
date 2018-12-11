0dc1be276c9e42706cd625ea3392cf2d6b640292
Hal Lee <hal@lee.me>
2018-12-11 06:55:53 -0500

    Custom decoder to ensure type

--- Sources/App/Models/GenericContentItem.swift
+++ Sources/App/Models/GenericContentItem.swift
@@ -11,9 +11,26 @@ struct GenericContentItem: Content, PostgreSQLUUIDModel, Migration {
 struct GenericContentField: Content {
     var name: String // "Title"
     var type: SupportedType
-    var value: SupportedValue? // .string("A Post Title")
-    var required = false
+    var value: SupportedValue // .string("A Post Title")
+    var required: Bool
     // TODO: Define how it's displayed
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        type = try container.decode(SupportedType.self, forKey: .type)
+        required = try container.decode(Bool.self, forKey: .required)
+        switch type {
+        case .String: value = SupportedValue.string(try? container.decode(String.self, forKey: .value))
+        case .Int: value = SupportedValue.int(try? container.decode(Int.self, forKey: .value))
+        case .Float: value = SupportedValue.float(try? container.decode(Float.self, forKey: .value))
+        case .Bool: value = SupportedValue.bool(try? container.decode(Bool.self, forKey: .value))
+        case .Date: value = SupportedValue.date(try? container.decode(Date.self, forKey: .value))
+        case .URL: value = SupportedValue.url(try? container.decode(URL.self, forKey: .value))
+        case .Array:
+            fatalError() // TODO
+        }
+    }
 }
 
 //struct SupportedTypeValue: Content, Equatable {
