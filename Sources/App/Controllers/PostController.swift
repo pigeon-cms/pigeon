ee155c5486e6d3fc1b21eaa3c0237000c7dd5721
Hal Lee <hal@lee.me>
2018-12-16 15:53:24 -0500

    Create post controller

--- Sources/App/Controllers/PostController.swift
+++ Sources/App/Controllers/PostController.swift
@@ -4,30 +4,37 @@ import Fluent
 class PostController: PigeonController {
 
     override func loginGuardedBoot(router: Router) throws {
-        router.get("/", String.parameter, use: postListController)
+        router.get("/", String.parameter, use: postViewController)
+        router.get([String.parameter, "/create"], use: createPostView)
     }
 
 }
 
 private extension PostController {
     
-    func postListController(_ request: Request) throws -> Future<View> {
+    func postViewController(_ request: Request) throws -> Future<View> {
         guard let typeName = try request.parameters.next(String.self).removingPercentEncoding else {
             throw Abort(.notFound)
         }
-        
-        return GenericContentCategory.query(on: request)
-                                     .filter(\.plural == typeName)
-                                     .first().flatMap { category in
-            guard let category = category else {
-                throw Abort(.notFound)
-            }
+
+        return try request.contentCategory(typePluralName: typeName).flatMap { category in
             return try self.generatePostListView(for: request,
                                                  posts: category.items ?? [],
                                                  plural: category.plural)
         }
     }
-    
+
+    func createPostView(_ request: Request) throws -> Future<View> {
+        guard let typeName = try request.parameters.next(String.self).removingPercentEncoding else {
+            throw Abort(.notFound)
+        }
+
+        return try request.contentCategory(typePluralName: typeName).flatMap { category in
+            return try self.generateCreatePostView(for: request,
+                                                   category: category)
+        }
+    }
+
     struct PostListPage: Codable {
         var shared: BasePage
         var posts: [GenericContentItem]
@@ -43,6 +50,32 @@ private extension PostController {
                                          posts: posts,
                                          plural: plural)
             return try request.view().render("Posts/posts", postsPage)
+        }
+    }
+
+    struct CreatePostPage: Codable {
+        var shared: BasePage
+        var category: GenericContentCategory
+    }
+
+    func generateCreatePostView(for request: Request,
+                                category: GenericContentCategory) throws -> Future<View> {
+        return try request.base().flatMap { basePage in
+            let createPostPage = CreatePostPage(shared: basePage, category: category)
+            return try request.view().render("Posts/create-post", createPostPage)
+        }
+    }
+}
+
+extension Request {
+    func contentCategory(typePluralName: String) throws -> Future<GenericContentCategory> {
+        return GenericContentCategory.query(on: self)
+                                     .filter(\.plural == typePluralName)
+                                     .first().map { category in
+            guard let category = category else {
+                throw Abort(.notFound)
+            }
+            return category
         }
     }
 }
