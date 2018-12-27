641828de841f03708a9e57ac773d6d7f65262823
Hal Lee <hal@hal-lee.com>
2018-12-27 09:42:00 -0500

    Added single quotes to stringy JS tags

--- Sources/App/Leaf/JSTag.swift
+++ Sources/App/Leaf/JSTag.swift
@@ -4,12 +4,24 @@ import Leaf
 /// Custom leaf tag that escapes Strings for single-quote JavaScript object contexts.
 /// For example, `"I'm a String"` outputs to `"I\'m a String"`.
 final class JSTag: TagRenderer {
+    
     init() { }
 
     func render(tag: TagContext) throws -> EventLoopFuture<TemplateData> {
-        guard let escaped = tag.parameters.first?.string?.replacingOccurrences(of: "'", with: "\\'") else {
-            return tag.container.future(tag.parameters.first ?? TemplateData.null)
+        var enclosingQuotes = true
+        if let _ = tag.parameters.first?.bool {
+            enclosingQuotes = false
         }
+        
+        guard var escaped = tag.parameters.first?.string?.replacingOccurrences(of: "'", with: "\\'") else {
+            return tag.container.future(TemplateData.string("''"))
+        }
+        
+        if enclosingQuotes {
+            escaped = "'\(escaped)'"
+        }
+        
         return tag.container.future(TemplateData.string(escaped))
     }
+
 }
