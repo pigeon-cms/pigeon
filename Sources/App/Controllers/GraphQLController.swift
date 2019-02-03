4cb34814c2b3644d9d7fdc5c87ea22f4b0bdf68e
Hal Lee <hal@lee.me>
2019-02-03 11:16:38 -0500

    Fixed schema creation to match types, still very basic

--- Sources/App/Controllers/GraphQLController.swift
+++ Sources/App/Controllers/GraphQLController.swift
@@ -4,8 +4,8 @@ import GraphQL
 class GraphQLController: PigeonController {
 
     override func authBoot(router: Router) throws {
-        router.post(["/graphql", String.parameter], use: graphQLPostQueryHandler)
-        router.get(["/graphql", String.parameter], use: graphQLGetQueryHandler)
+        router.post(["/graphql"], use: graphQLPostQueryHandler)
+        router.get(["/graphql"], use: graphQLGetQueryHandler)
     }
 
 }
@@ -14,27 +14,35 @@ private extension GraphQLController {
 
     struct GraphQLHTTPBody: Codable {
         var query: String
-//        var variables: [String: Any]? // TODO: codable representation of "any" json type
+        var variables: [String: SupportedValue]? // TODO: codable representation of "any" json type
+    }
+
+    func schema(_ request: Request) throws -> Future<GraphQLSchema> {
+        return request.allContentTypes().map { contentTypes in
+            var rootFields = [String: GraphQLField]()
+            for type in contentTypes {
+                rootFields[type.plural.camelCase()] = GraphQLField(type: try type.graphQLType(), resolve: { (source, args, context, eventLoopGroup, info) -> EventLoopFuture<Any?> in
+                    return eventLoopGroup.next().newSucceededFuture(result: try type.graphQLType())
+                })
+            }
+            let schema = try GraphQLSchema(
+                query: GraphQLObjectType(
+                        name: "RootQueryType",
+                        fields: rootFields)
+            )
+            return schema
+        }
     }
 
     func graphQLPostQueryHandler(_ request: Request) throws -> Future<Response> {
-        guard let typeName = try request.parameters.next(String.self).removingPercentEncoding else {
-            throw Abort(.notFound)
-        }
-
         guard let json = try? request.content.decode(json: GraphQLHTTPBody.self, using: JSONDecoder()) else {
             throw Abort(.badRequest)
         }
 
         return json.flatMap { query in
-
-            return try request.contentCategory(type: typeName).flatMap { category in
-                let queryString = query.query
-
-                let schema = try category.createGraphQLSchema()
-
-                let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-                return try graphql(schema: schema, request: queryString, eventLoopGroup: eventLoopGroup).map { map in
+            return try self.schema(request).flatMap { schema in
+                let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1) // TODO: is this necessary
+                return try graphql(schema: schema, request: query.query, eventLoopGroup: eventLoopGroup).map { map in
                     let map = try map.asMap()
                     let data = "\(map)".data(using: .utf8)
                     return Response(http: HTTPResponse.init(status: .ok, body: data!),
@@ -49,7 +57,7 @@ private extension GraphQLController {
             var path: String
         }
         if request.http.accept.contains(where: { $0.mediaType.type == "text" && $0.mediaType.subType == "html" }) {
-            return try request.view().render("graphiql", GraphiQLPage(path: "/graphql/Status%20Messages")) // TODO: query not hardcoded
+            return try request.view().render("graphiql", GraphiQLPage(path: "/graphql")) // TODO: query not hardcoded
         }
         fatalError("TODO: Need to implement GET GraphQL queries")
     }
--- Sources/App/Models/ContentCategory.swift
+++ Sources/App/Models/ContentCategory.swift
@@ -12,23 +12,19 @@ final class ContentCategory: Content, PostgreSQLUUIDModel, Migration {
     var template: [ContentField]
     // var accessLevel: SomeEnum // TODO: access level for api content
 
-    func createGraphQLSchema() throws -> GraphQLSchema {
-        let schema = try GraphQLSchema(
-            query: GraphQLObjectType(
-                name: "RootQueryType",
-                fields: graphQLFields()
-            )
-        )
-        return schema
-    }
-
     func graphQLFields() -> [String: GraphQLField] {
         var fields = [String: GraphQLField]()
         for field in template {
-            fields[field.name] = GraphQLField(type: field.type.graphQL, resolve: { (source, args, context, eventLoopGroup, info) -> EventLoopFuture<Any?> in
+            fields[field.name.camelCase()] = GraphQLField(type: field.type.graphQL, resolve: { (source, args, context, eventLoopGroup, info) -> EventLoopFuture<Any?> in
+                // TODO: actually fetch stuff
                 return eventLoopGroup.next().newSucceededFuture(result: "Hello world")
             })
         }
         return fields
+    }
+
+    func graphQLType() throws -> GraphQLOutputType {
+        let type = try GraphQLObjectType(name: plural.pascalCase(), fields: graphQLFields())
+        return type
     }
 }
