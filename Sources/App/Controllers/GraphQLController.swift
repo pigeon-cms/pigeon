8812d2af765ca23413e122328051a6f9f47caf00
Hal Lee <hal@lee.me>
2019-02-03 07:10:35 -0500

    Working hello world query

--- Sources/App/Controllers/GraphQLController.swift
+++ Sources/App/Controllers/GraphQLController.swift
@@ -11,43 +11,44 @@ class GraphQLController: PigeonController {
 
 private extension GraphQLController {
 
+    struct GraphQLHTTPBody: Codable {
+        var query: String
+//        var variables: [String: Any]? // TODO: codable representation of "any" json type
+    }
+
     func graphQLHandler(_ request: Request) throws -> Future<Response> {
         guard let typeName = try request.parameters.next(String.self).removingPercentEncoding else {
             throw Abort(.notFound)
         }
 
-        guard let queryData = request.http.body.data,
-            let queryString = String(data: queryData, encoding: .utf8) else {
-                throw Abort(.badRequest)
+        guard let json = try? request.content.decode(json: GraphQLHTTPBody.self, using: JSONDecoder()) else {
+            throw Abort(.badRequest)
         }
 
-        let schema = try GraphQLSchema(
-            query: GraphQLObjectType(
-                name: "RootQueryType",
-                fields: [
-                    "hello": GraphQLField(
-                        type: GraphQLString,
-                        resolve: { _, _, _, eventLoopGroup, _ in
-                            return eventLoopGroup.next().newSucceededFuture(result: typeName)
-                    })
-                ]
+        return json.flatMap { query in
+            let queryString = query.query
+
+            let schema = try GraphQLSchema(
+                query: GraphQLObjectType(
+                    name: "RootQueryType",
+                    fields: [
+                        "hello": GraphQLField(
+                            type: GraphQLString,
+                            resolve: { _, _, _, eventLoopGroup, _ in
+                                return eventLoopGroup.next().newSucceededFuture(result: typeName)
+                        })
+                    ]
+                )
             )
-        )
 
-        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        return try graphql(schema: schema, request: queryString, eventLoopGroup: eventLoopGroup).map { map in
-            let map = try map.asMap()
-            let data = "\(map)".data(using: .utf8)
-            return Response(http: HTTPResponse.init(status: .ok, body: data!),
-                            using: request.sharedContainer)
+            let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+            return try graphql(schema: schema, request: queryString, eventLoopGroup: eventLoopGroup).map { map in
+                let map = try map.asMap()
+                let data = "\(map)".data(using: .utf8)
+                return Response(http: HTTPResponse.init(status: .ok, body: data!),
+                                using: request.sharedContainer)
+            }
         }
-
-        //        return try request.contentCategory(type: typeName).flatMap { category in
-        //            return try category.items.query(on: request).paginate(for: request).map { content in
-        //                let publicData = content.data.map { return ContentItemPublic($0) }
-        //                return Paginated<ContentItemPublic>(page: content.page, data: publicData)
-        //            }
-        //        }
     }
     
 }
