bc7778e08ed50d9896b654c6224c21ffb7bac73d
Hal Lee <hal@lee.me>
2019-02-03 07:44:56 -0500

    Neat conversion from SupportedType to GraphQLType

---
+++ Sources/App/Models/GraphQLTypes.swift
@@ -0,0 +1,16 @@
+import GraphQL
+
+extension SupportedType {
+    var graphQL: GraphQLType {
+        switch self {
+        case .string: return GraphQLString
+        case .int: return GraphQLInt
+        case .float: return GraphQLFloat
+        case .bool: return GraphQLBoolean
+        case .date: return GraphQLString
+        case .url: return GraphQLString
+        case .array(let type):
+            return GraphQLList(type.graphQL)
+        }
+    }
+}
