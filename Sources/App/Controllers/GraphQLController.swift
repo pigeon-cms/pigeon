8ff3078ffef23f8cd2c2bb43f52cda2a74e54ffc
Hal Lee <hal@lee.me>
2019-02-03 08:29:43 -0500

    GraphiQL

--- Sources/App/Controllers/GraphQLController.swift
+++ Sources/App/Controllers/GraphQLController.swift
@@ -4,7 +4,8 @@ import GraphQL
 class GraphQLController: PigeonController {
 
     override func authBoot(router: Router) throws {
-        router.post(["/graphql", String.parameter], use: graphQLHandler)
+        router.post(["/graphql", String.parameter], use: graphQLPostQueryHandler)
+        router.get(["/graphql", String.parameter], use: graphQLGetQueryHandler)
     }
 
 }
@@ -16,7 +17,7 @@ private extension GraphQLController {
 //        var variables: [String: Any]? // TODO: codable representation of "any" json type
     }
 
-    func graphQLHandler(_ request: Request) throws -> Future<Response> {
+    func graphQLPostQueryHandler(_ request: Request) throws -> Future<Response> {
         guard let typeName = try request.parameters.next(String.self).removingPercentEncoding else {
             throw Abort(.notFound)
         }
@@ -26,29 +27,31 @@ private extension GraphQLController {
         }
 
         return json.flatMap { query in
-            let queryString = query.query
 
-            let schema = try GraphQLSchema(
-                query: GraphQLObjectType(
-                    name: "RootQueryType",
-                    fields: [
-                        "hello": GraphQLField(
-                            type: GraphQLString,
-                            resolve: { _, _, _, eventLoopGroup, _ in
-                                return eventLoopGroup.next().newSucceededFuture(result: typeName)
-                        })
-                    ]
-                )
-            )
+            return try request.contentCategory(type: typeName).flatMap { category in
+                let queryString = query.query
 
-            let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-            return try graphql(schema: schema, request: queryString, eventLoopGroup: eventLoopGroup).map { map in
-                let map = try map.asMap()
-                let data = "\(map)".data(using: .utf8)
-                return Response(http: HTTPResponse.init(status: .ok, body: data!),
-                                using: request.sharedContainer)
+                let schema = try category.createGraphQLSchema()
+
+                let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+                return try graphql(schema: schema, request: queryString, eventLoopGroup: eventLoopGroup).map { map in
+                    let map = try map.asMap()
+                    let data = "\(map)".data(using: .utf8)
+                    return Response(http: HTTPResponse.init(status: .ok, body: data!),
+                                    using: request.sharedContainer)
+                }
             }
         }
+    }
+
+    func graphQLGetQueryHandler(_ request: Request) throws -> Future<View> {
+        struct GraphiQLPage: Codable {
+            var path: String
+        }
+        if request.http.accept.contains(where: { $0.mediaType.type == "text" && $0.mediaType.subType == "html" }) {
+            return try request.view().render("graphiql", GraphiQLPage(path: "/graphql/Status%20Messages")) // TODO: query not hardcoded
+        }
+        fatalError("TODO: Need to implement GET GraphQL queries")
     }
     
 }
