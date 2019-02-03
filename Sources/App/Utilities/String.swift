b61f4ec6693ed24fefbd4126e2100be0fe72e69c
Hal Lee <hal@lee.me>
2019-02-03 11:16:22 -0500

    Improved camelCase and PascalCase conversion

--- Sources/App/Utilities/String.swift
+++ Sources/App/Utilities/String.swift
@@ -16,14 +16,37 @@ extension String {
         }
 
         return whiteSpaceSplit.reduce("") { phrase, word -> String in
-            var word = word.lowercased()
-            word = word.filter { return allowedCharacters.contains($0) }
-            if phrase != "" {
-                word = word.capitalized
+            var word = word.filter { return allowedCharacters.contains($0) }
+
+            if phrase == "" {
+                /// first word in the sequence
+                word = word.lowercased()
+            } else {
+                if word != word.uppercased() && word != word.iosStyle() {
+                    word = word.capitalized
+                }
             }
+
             return phrase + word
         }
 
+    }
+
+    /// Converts this string to PascalCase. Can convert sentence formatted, snake_case, and pascal-case.
+    /// Only alphanumerics are allowed.
+    func pascalCase() -> String {
+        var camelCase = self.camelCase()
+        guard let first = camelCase.first else { return camelCase }
+        return String(first).uppercased() + camelCase.dropFirst()
+    }
+
+    /// This string with "iOS" style capitalization, with the first character
+    /// lowercase and the rest uppercase.
+    func iosStyle() -> String {
+        guard self.count > 1 else { return self }
+        guard let first = self.first else { return self }
+
+        return String(first).lowercased() + self.dropFirst().uppercased()
     }
 
 }
