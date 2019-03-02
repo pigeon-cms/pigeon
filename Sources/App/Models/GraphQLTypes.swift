d4a9c7c1a7653f1e5d514232ee401436e18576be
Hal Lee <hal@lee.me>
2019-03-02 17:25:17 -0500

    Added Markdown type to GraphQL

--- Sources/App/Models/GraphQLTypes.swift
+++ Sources/App/Models/GraphQLTypes.swift
@@ -1,9 +1,11 @@
+import Vapor
 import GraphQL
 
 extension SupportedType {
     var graphQL: GraphQLOutputType {
         switch self {
-        case .string, .markdown: return GraphQLString
+        case .markdown: return SupportedType.graphQLMarkdownType
+        case .string: return GraphQLString
         case .int: return GraphQLInt
         case .float: return GraphQLFloat
         case .bool: return GraphQLBoolean
@@ -13,6 +15,24 @@ extension SupportedType {
             return GraphQLList(type.graphQL)
         }
     }
+
+    private static var graphQLMarkdownType: GraphQLOutputType {
+        let fields = [
+            "html": GraphQLField(type: GraphQLString, resolve: { (source, args, context, eventLoopGroup, info) -> EventLoopFuture<Any?> in
+                guard let markdown = source as? Markdown else {
+                    return eventLoopGroup.future(nil)
+                }
+                return eventLoopGroup.future(markdown.html)
+            }),
+            "markdown": GraphQLField(type: GraphQLString, resolve: { (source, args, context, eventLoopGroup, info) -> EventLoopFuture<Any?> in
+                guard let markdown = source as? Markdown else {
+                    return eventLoopGroup.future(nil)
+                }
+                return eventLoopGroup.future(markdown.markdown)
+            })]
+        return try! GraphQLObjectType(name: "Markdown",
+                                      fields: fields)
+    }
 }
 
 extension SupportedValue {
@@ -20,6 +40,7 @@ extension SupportedValue {
         switch self {
         case .string(let value): return value as Any
         case .bool(let value): return value as Any
+        case .markdown(let value): return value as Any
         default:
             fatalError()
         }
