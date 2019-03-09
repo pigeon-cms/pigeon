e64ab611b72ecd8ea902e1fbe327dfba1c91d2f3
Hal Lee <hal@lee.me>
2019-03-09 12:31:59 -0500

    Not worth figuring out associated values

--- Sources/App/Models/ContentState.swift
+++ Sources/App/Models/ContentState.swift
@@ -1,29 +1,8 @@
 import Vapor
 import FluentPostgreSQL
 
-enum ContentState: Content, ReflectionDecodable, Equatable, RawRepresentable  {
-    typealias RawValue = String
-
+enum ContentState: String, Equatable, Content, PostgreSQLEnum, PostgreSQLMigration {
     case draft
-    /// TODO: scheduled(Date)
+    case scheduled
     case published
-
-    init?(rawValue: RawValue) {
-        switch rawValue {
-        case "draft": self = .draft
-        case "published": self = .published
-        default: self = .draft
-        }
-    }
-
-    var rawValue: RawValue {
-        switch self {
-        case .draft: return "draft"
-        case .published: return "published"
-        }
-    }
-
-    static func reflectDecoded() throws -> (ContentState, ContentState) {
-        return (.draft, .published)
-    }
 }
