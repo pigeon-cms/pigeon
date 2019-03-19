e71e9df7048981b48cdf44fe06cd65410d0dea75
Hal Lee <hal@lee.me>
2019-03-19 07:42:28 -0400

    More punishing test

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd Benchmarking
-vegeta attack -rate=60 -duration=6s -timeout=10s -targets=./vegeta.txt | vegeta report
+vegeta attack -rate=100 -duration=14s -timeout=10s -targets=./vegeta.txt | vegeta report
